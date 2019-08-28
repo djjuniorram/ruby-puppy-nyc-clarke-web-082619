67ee8270c43d4341ed32ae7b5bfe3c814c056862
github username <github email address>
2019-08-28 13:47:54 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,27 @@
-# Add your code here
 class Dog
-
-    @@all = []
-
     attr_accessor :name
-
+  
+    @@all = []
+  
     def initialize(name)
-        @name = name
-        @save = save
+      @name = name
+      save
     end
-
+  
     def self.all
-        @@all
+      @@all
     end
-
+  
     def self.print_all
-        @@all.each do |dog|
-            puts dog.name
-        end
+      # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+      puts @@all.map{ |dog| dog.name }
     end
-
-    def save
-        @@all << self
-    end
-
+  
     def self.clear_all
-        @@all.clear
+      @@all.clear
     end
-end
+  
+    def save
+      @@all << self
+    end
+  end
